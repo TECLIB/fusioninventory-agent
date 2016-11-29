df180776811d95aca1dd340f5ee7460e4ebb4103
tlornet <tlornet@teclib.com>
2016-11-29 11:50:11 +0100

    variant...

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -252,7 +252,7 @@ sub _getRegistryValueFromWMI {
         "root\\default"
     );
     my $objReg = $WMIService->Get("StdRegProv");
-#    Win32::OLE::Variant->use(qw/VT_BYREF VT_BSTR/);
+    Win32::OLE::Variant->use(qw/VT_BYREF VT_BSTR/);
     my $result = Win32::OLE::Variant(VT_BYREF()|VT_BSTR(),0);
     my $return = $objReg->GetStringValue($hkey, $params{keyName}, $params{valueName}, $result);
     if ($return != 0) {
