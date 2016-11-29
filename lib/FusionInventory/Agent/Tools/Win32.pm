b2f6d21409138cd9fae892b54113fd596c33e709
tlornet <tlornet@teclib.com>
2016-11-29 11:31:18 +0100

    variant merding...

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -252,8 +252,8 @@ sub _getRegistryValueFromWMI {
         "root\\default"
     );
     my $objReg = $WMIService->Get("StdRegProv");
-    Win32::OLE::Variant->use('VT_BYREF');
-    my $result = Variant(VT_BYREF|VT_BSTR,0);
+    Win32::OLE::Variant->import('VT_BYREF');
+    my $result = Variant(VT_BYREF()|VT_BSTR(),0);
     my $return = $objReg->GetStringValue($hkey, $params{keyName}, $params{valueName}, $result);
     if ($return != 0) {
         $result = undef;
