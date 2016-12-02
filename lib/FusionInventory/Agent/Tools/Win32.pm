651257616bb68d9a437ffc39a97fba1585df06c8
tlornet <tlornet@teclib.com>
2016-12-02 12:22:26 +0100

    hard debugging !

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -228,10 +228,23 @@ sub getRegistryValue {
 }
 
 sub getRegistryValueFromWMI {
+    my (%params) = @_;
+
+    if ($params{path} =~ m{^(HKEY_\S+)/(.+)/([^/]+)} ) {
+        $params{root}      = $1;
+        $params{keyName}   = $2;
+        $params{valueName} = $3;
+    } else {
+        $params{logger}->error(
+            "Failed to parse '$params{path}'. Does it start with HKEY_?"
+        ) if $params{logger};
+        return;
+    }
+
     my $win32_ole_dependent_api = {
         array => 1,
         funct => '_getRegistryValueFromWMI',
-        args  => \@_
+        args  => \%params
     };
 
     return _call_win32_ole_dependent_api($win32_ole_dependent_api);
@@ -241,28 +254,16 @@ sub _getRegistryValueFromWMI {
     my (%params) = @_;
 
     FusionInventory::Agent::Logger::File->require();
-
-    my ($root, $keyName, $valueName);
-    if ($params{path} =~ m{^(HKEY_\S+)/(.+)/([^/]+)} ) {
-        $root      = $1;
-        $keyName   = $2;
-        $valueName = $3;
-    } else {
-        $params{logger}->error(
-            "Failed to parse '$params{path}'. Does it start with HKEY_?"
-        ) if $params{logger};
-        return;
-    }
-
 #    Win32::Registry->require();
 
     my $hkey;
-    if ($root =~ /^HKEY_LOCAL_MACHINE(?:\\|\/)(.*)$/) {
+    if ($params{root} =~ /^HKEY_LOCAL_MACHINE(?:\\|\/)(.*)$/) {
         $hkey = $Win32::Registry::HKEY_LOCAL_MACHINE;
-        my $keyName = $1 . '/' . $keyName;
+        my $keyName = $1 . '/' . $params{keyName};
         $keyName =~ tr#/#\\#;
+        $params{keyName} = $keyName;
     }
-    my $dd = Data::Dumper->new([\%params, \$hkey, {root => $root, keyName => $keyName, valueName => $valueName}]);
+    my $dd = Data::Dumper->new([\%params, \$hkey]);
     $params{logger}->debug2($dd->Dump) if $params{logger};
 
     my $WMIService = _connectToService(
@@ -284,7 +285,7 @@ sub _getRegistryValueFromWMI {
 #    Win32::OLE::Variant->require();
     my $result = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_BYREF()|Win32::OLE::Variant::VT_BSTR(),0);
     $params{logger}->debug2('result variant created') if $params{logger};
-    my $return = $objReg->GetStringValue($hkey, $keyName, $valueName, $result);
+    my $return = $objReg->GetStringValue($hkey, $params{keyName}, $params{valueName}, $result);
     $params{logger}->debug2('result : ' . $result . ' - ' . 'return : ' . $return) if $params{logger};
 
     return $result;
