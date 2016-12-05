f2ac590f0f535671433c4fd79cb5e3542e6cd334
tlornet <tlornet@teclib.com>
2016-12-05 11:00:48 +0100

    fuck !

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -284,9 +284,9 @@ sub _getRegistryValueFromWMI {
     my $result = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_BYREF()|Win32::OLE::Variant::VT_BSTR(),0);
     $params{logger}->debug2('result variant created') if $params{logger};
     my $return = $objReg->GetStringValue($hkey, $params{keyName}, $params{valueName}, $result);
-    $params{logger}->debug2('result : ' . $result . ' - ' . 'return : ' . $return . ' - ref result: ' . ref($result)) if $params{logger};
-
-    return $result;
+    my $value = $result;
+    $params{logger}->debug2('result : ' . $result . ' - ' . 'return : ' . $return . ' - ref result: ' . ref($result) . ' - ref value ' . ref($value)) if $params{logger};
+    return $value;
 }
 
 sub isDefinedRemoteRegistryKey {
