d4819244fbf0f5fbfbec2abf633deaf3874d59a4
tlornet <tlornet@teclib.com>
2016-12-05 15:21:10 +0100

    VT_ARRAY dimension

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -402,7 +402,8 @@ sub _getRegistryKeyFromWMI{
     my $return = $objReg->EnumKey($hkey, $params{keyName}, $keys);
     my $value = sprintf(ref($keys) . ' _ ' . $return);
 
-    $value .= $keys->Value();
+    my @keys = $keys->Value();
+    $value .= ' - ' . join(' ', @keys);
 
     return $value;
 }
