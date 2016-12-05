8382e691c12d8063ddb6d0c0055fba6e4c4d2b17
tlornet <tlornet@teclib.com>
2016-12-05 14:58:41 +0100

    VT_ARRAY dimension

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -398,7 +398,7 @@ sub _getRegistryKeyFromWMI{
         $params{keyName} = $keyName;
     }
 
-    my $keys = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_ARRAY() | Win32::OLE::Variant::VT_BYREF() | Win32::OLE::Variant::VT_VARIANT());
+    my $keys = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_ARRAY() | Win32::OLE::Variant::VT_BSTR(), [1, 1024]);
     my $return = $objReg->EnumKey($hkey, $params{keyName}, $keys);
     my $value = sprintf(ref($keys) . ' _ ' . $return);
 
