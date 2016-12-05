0aa7732c2ae3e34f1294a4fca4db92c90112b57a
tlornet <tlornet@teclib.com>
2016-12-05 16:33:58 +0100

    VT_ARRAY... extracting values....

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -401,7 +401,7 @@ sub _getRegistryKeyFromWMI{
     }
 
 #    my $keys = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_VARIANT() | Win32::OLE::Variant::VT_ARRAY(), [1, 1024]);
-    my $keys = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_ARRAY() | Win32::OLE::Variant::VT_BSTR() | Win32::OLE::Variant::VT_BYREF(), [1, 1] );
+    my $keys = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_ARRAY() | Win32::OLE::Variant::VT_BSTR() , [1, 1] );
     my $return = $objReg->EnumKey($hkey, $params{keyName}, $keys);
     my $value = sprintf(ref($keys) . ' - type : ' . $keys->Type . ' - return : ' . $return);
 
