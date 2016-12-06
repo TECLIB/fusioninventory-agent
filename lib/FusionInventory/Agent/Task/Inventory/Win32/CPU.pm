dc3c074c032f7a8a6ac5692526f3ffdce1c11f86
tlornet <tlornet@teclib.com>
2016-12-06 14:41:10 +0100

    cpu remote wmi

--- lib/FusionInventory/Agent/Task/Inventory/Win32/CPU.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Win32/CPU.pm
@@ -145,7 +145,7 @@ sub _retrieveCpuIdFromRemoteRegistry {
 
     return unless $params{WMIService};
 
-    my $cpuIdPath = $path . '/' . $cpuId . '/';
+    my $cpuIdPath = $path . '/' . "$cpuId" . 'eh';
     my $cpuIdKeys = getRegistryKey(
         path => $cpuIdPath,
         %params
