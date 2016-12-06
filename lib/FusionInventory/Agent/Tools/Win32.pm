f8d1eb5184fee23743e5e9d8c4fa81cd331be490
tlornet <tlornet@teclib.com>
2016-12-06 15:12:26 +0100

    cpu remote wmi

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -420,7 +420,6 @@ sub _getRegistryKeyFromWMI{
 
     return unless defined $return && $return == 0;
     my $subKeys = [];
-    return ['juste avant foreach'];
     foreach my $item ( in( $arr->Value ) ) {
         push @$subKeys, $item;
     } # end foreach
