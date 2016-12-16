9cfcd406e277273f5438b5ca8f140f74d88b3f06
tlornet <tlornet@teclib.com>
2016-12-16 16:50:52 +0100

    again testing by hard debugging...

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -700,7 +700,8 @@ sub _retrieveRemoteRegistryValueByType {
             $value .= ' - ' . $result->Value();
             $value .= ' - ' . sprintf($result);
         }
-        $result = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_BYREF() | Win32::OLE::Variant::VT_BSTR(), 0);
+        $result = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_I4(), 0);
+#        $result = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_BYREF() | Win32::OLE::Variant::VT_BSTR(), 0);
         $value = $params{objReg}->GetStringValue($params{hkey}, $params{keyName}, $params{valueName}, $result);
     } elsif ($params{valueType} == REG_EXPAND_SZ) {
         $value = $params{objReg}->GetExpandedStringValue($params{hkey}, $params{keyName}, $params{valueName}, $result);
