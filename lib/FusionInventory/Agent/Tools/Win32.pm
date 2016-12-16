75a54cd1ed9277a77c7c54f5111aa57bc45e2825
tlornet <tlornet@teclib.com>
2016-12-16 15:08:44 +0100

    hard debugging now !!!!!

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -718,7 +718,7 @@ sub _retrieveRemoteRegistryValueByType {
 
     open (O, ">>" . 'eval_return.log');
     print O 'valueType : ' . $params{valueType};
-    print O ' ?== ' . Win32::Registry::REG_DWORD() . "\n";
+    print O ' ?== ' . REG_DWORD . "\n";
     print O ' ?== ' . Win32::Registry::REG_BINARY() . "\n";
     print O ' ?== ' . Win32::Registry::REG_EXPAND_SZ() . "\n";
     print O ' ?== ' . Win32::Registry::REG_MULTI_SZ() . "\n";
