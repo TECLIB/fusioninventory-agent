deb8be8ed50ebb139c45f1880bec18bdb98c5ad9
tlornet <tlornet@teclib.com>
2016-12-16 12:20:05 +0100

    testing again and again

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -653,6 +653,7 @@ sub _retrieveValuesNameAndType {
         close O;
         my $return = $params{objReg}->EnumValues($hkey, $params{keyName}, $arrValueNames, $arrValueTypes);
         print 'error : ' . $return . "\n";
+        print Win32::OLE::LastError . "\n";
         my $sprintfError = '';
         if (Win32::OLE->LastError) {
             open(O, ">>" . 'hard_debug.log');
