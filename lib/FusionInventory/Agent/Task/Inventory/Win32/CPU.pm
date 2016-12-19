a0c0bf949fd4e70970cbd8ba8229ef762735eae2
tlornet <tlornet@teclib.com>
2016-12-19 11:43:27 +0100

    good type

--- lib/FusionInventory/Agent/Task/Inventory/Win32/CPU.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Win32/CPU.pm
@@ -64,6 +64,7 @@ sub _getCPUs {
 
     my $val = getRegistryValue(
         path => "HKEY_LOCAL_MACHINE/Hardware/Description/System/CentralProcessor/0/Identifier",
+        valueType => FusionInventory::Agent::Tools::Win32::REG_SZ,
         %params
     );
     $params{logger}->debug2('retour de getRegistryKey "HKEY_LOCAL_MACHINE/Hardware/Description/System/CentralProcessor/0/Identifier" : ' . $val);
