2951868ce832ff724ea17b0a6721245c4bb2909f
tlornet <tlornet@teclib.com>
2016-12-20 12:34:45 +0100

    timeout too short to do networks inventory module. Should be longest just for networks, impossible for the moment. Should we do that enhancement ?

--- lib/FusionInventory/Agent/Config.pm
+++ lib/FusionInventory/Agent/Config.pm
@@ -12,7 +12,7 @@ require FusionInventory::Agent::Tools;
 
 my $default = {
     'additional-content'      => undef,
-    'backend-collect-timeout' => 180,
+    'backend-collect-timeout' => 600,
     'ca-cert-dir'             => undef,
     'ca-cert-file'            => undef,
     'color'                   => undef,
