c80db63584e4c6cd0e557a7773ec960ed532241b
tlornet <tlornet@teclib.com>
2016-12-21 10:10:14 +0100

    perl debugging (i mean not hard debugging)

--- lib/FusionInventory/Agent/Task/Inventory/Win32/Networks.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Win32/Networks.pm
@@ -109,6 +109,7 @@ sub _getDataFromRemote {
         %$wmiParams
     );
     my $data = {};
+    $DB::single = 1;
     foreach my $subkey_name (@$subKeys) {
         # skip variables
         next if $subkey_name =~ m{^/}
