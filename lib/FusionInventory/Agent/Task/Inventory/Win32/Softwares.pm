1ac2906e2a3ce51b04011f6b37d8d0e7c4b932b9
tlornet <tlornet@teclib.com>
2016-12-22 16:35:01 +0100

    uh

--- lib/FusionInventory/Agent/Task/Inventory/Win32/Softwares.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Win32/Softwares.pm
@@ -32,7 +32,7 @@ sub doInventory {
 
     my $wmiParams = {};
     $wmiParams->{WMIService} = $params{inventory}->{WMIService} ? $params{inventory}->{WMIService} : undef;
-    if ($wmiParams{WMIService}) {
+    if ($wmiParams->{WMIService}) {
         my $softwaresFromRemote = _retrieveSoftwareFromRemoteRegistry(
             %params,
             is64bit   => 1
