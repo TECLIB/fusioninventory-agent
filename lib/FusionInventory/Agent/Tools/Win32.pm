83d253381ba6fa7aa7594ba86a0d44712ff40fea
tlornet <tlornet@teclib.com>
2016-12-27 14:39:02 +0100

    test sigtrap

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -10,7 +10,7 @@ use threads 'exit' => 'threads_only';
 use threads::shared;
 
 #use sigtrap 'handler', \&errorHandler, 'error-signals';
-use sigtrap qw(handler errorHandler error-signals);
+#use sigtrap qw(handler errorHandler error-signals);
 use sigtrap qw(handler errorHandler old-interface-signals);
 
 use UNIVERSAL::require();
