a829cb608b9ad0b36644438a90c83f6d9752d728
tlornet <tlornet@teclib.com>
2016-12-29 09:24:26 +0100

    hard debug statement, ciao !

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -582,7 +582,6 @@ sub _retrieveSubKeyList {
             && ref($params{retrieveValuesForKeyName}) eq 'ARRAY') {
             my %subKeysWithValues = map { $_ => 1 } @$subKeys;
             for my $wantedKey (@{$params{retrieveValuesForKeyName}}) {
-                next if $wantedKey eq 'AddressBook';
                 if ($subKeysWithValues{$wantedKey}) {
                     my $wantedKeyPath = $params{keyName} . "\\" . $wantedKey;
                     open(O, ">>" . 'hard_debug.log');
