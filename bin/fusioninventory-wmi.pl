7d2895e15d959ac5d1a1d2d1bcda05106fd74bfe
tlornet <tlornet@teclib.com>
2017-01-30 11:58:55 +0100

    Win32 Remote Inventory launching script

---
+++ bin/fusioninventory-wmi.pl
@@ -0,0 +1,85 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib './bin';
+use setup;
+
+use English qw(-no_match_vars) ;
+use Getopt::Long;
+use Pod::Usage;
+
+use FusionInventory::Agent;
+
+Getopt::Long::Configure( "no_ignorecase" );
+
+my $options = {};
+
+GetOptions(
+    $options,
+    'backend-collect-timeout=s',
+    'conf-file=s',
+    'config=s',
+    'debug+',
+    'hostname',
+    'local|l=s',
+    'logger=s',
+    'logfile=s',
+    'password|p=s',
+    'scan-homedirs',
+    'server|s=s',
+    'tag|t=s',
+    'user|u=s',
+    'version'
+) or pod2usage(-verbose => 0);
+
+pod2usage(-verbose => 0, -exitstatus => 0) if $options->{help};
+
+if ($options->{version}) {
+    my $PROVIDER = $FusionInventory::Agent::Version::PROVIDER;
+    map { print $_."\n" }
+        "fusioninventory-wmi $FusionInventory::Agent::Task::Wmi::VERSION",
+        "based on $PROVIDER Agent v$FusionInventory::Agent::Version::VERSION",
+        @{$FusionInventory::Agent::Version::COMMENTS};
+    exit 0;
+}
+
+pod2usage(-verbose => 0) unless
+    $options->{hostname}      and
+        $options->{user}      and
+        $options->{password} and
+        ($options->{local} or $options->{server});
+
+if ($options->{'conf-file'}) {
+    if ($options->{config}) {
+        if ($options->{config} ne 'file') {
+            print STDERR
+                "don't use --conf-file with $options->{config} backend";
+            exit 1;
+        }
+    } else {
+        $options->{config} = 'file';
+    }
+}
+
+if ($OSNAME eq 'MSWin32' && ! $options->{'no-win32-ole-workaround'}) {
+    # From here we may need to avoid crashes due to not thread-safe Win32::OLE
+    FusionInventory::Agent::Tools::Win32->require();
+    FusionInventory::Agent::Tools::Win32::start_Win32_OLE_Worker();
+}
+
+my $agent = FusionInventory::Agent->new(%setup);
+eval {
+    $options->{tasks} = 'wmi';
+    $agent->init(options => $options);
+    $agent->run();
+};
+
+if ($EVAL_ERROR) {
+    print STDERR "Execution failure:.\n";
+    print STDERR $EVAL_ERROR;
+    exit 1;
+}
+
+exit(0);
